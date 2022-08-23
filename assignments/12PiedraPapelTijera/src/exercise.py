790a740482d97b7a980e63d67614b9ea9a0d9f87
jeshugui <110926417+jeshugui@users.noreply.github.com>
2022-08-22 20:55:19 -0500

    Update exercise.py

--- assignments/12PiedraPapelTijera/src/exercise.py
+++ assignments/12PiedraPapelTijera/src/exercise.py
@@ -1,6 +1,30 @@
 def main():
     # Escribe tu código abajo de esta línea
+    tA = str(input('Tirada de Ana: '))
+    tJ = str(input('Tirada de Juan: '))
     
+    if len(tA)!= 1 or len(tJ) != 1:
+        print ("Las tiradas deben ser un carácter")
+    elif tA == str('a') and tJ == str('t'):
+        print('Gana Ana')
+    elif tA == str('a') and tJ == str('p'):
+        print('Gana Juan')
+    elif tA == str('a') and tJ == str('a'):
+        print('Empate')
+    elif tA == str('t') and tJ == str('p'):
+        print('Gana Ana')
+    elif tA == str('t') and tJ == str('t'):
+        print('Empate')
+    elif tA == str('p') and tJ == str('p'):
+        print('Empate')
+    elif tA == str('p') and tJ == str('a'):
+        print('Gana Ana')
+    elif tA == str('t') and tJ == str('a'):
+        print('Gana Juan')
+    elif tA == str('p') and tJ == str('t'):
+        print('Gana Juan')
+    else:
+        print('Tirada incorrecta')
 
 if __name__ == '__main__':
     main()
